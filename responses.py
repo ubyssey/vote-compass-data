ff16de1d793faeba6186eed42c9c1abc31d97918
C Lin <CYLin@dhcp-206-87-194-217.ubcsecure.wireless.ubc.ca>
2017-03-21 13:18:18 -0700

    count responses

--- responses.py
+++ responses.py
@@ -6,9 +6,6 @@ def get_responses_as_list():
 
         reader = csv.reader(csv_file)
 
-        # Skip the header row
-        next(reader)
-
         return list(reader)
 
 responses = get_responses_as_list()
@@ -22,13 +19,42 @@ def before_cutoff_date(date):
     return response_date < cutoff_date
 
 def num_skipped(answers):
-    return len(filter(lambda x: x == '', answers))
+    return len(list(filter(lambda x: x == '', answers)))
 
 def is_valid_response(row):
     return num_skipped(row[1:]) < 10 and before_cutoff_date(row[0])
 
-print len(responses)
+print (len(responses))
 
-responses = filter(is_valid_response, responses)
+questions = responses[0][1:]
 
-print len(responses)
+responses = list(filter(is_valid_response, responses[1:]))
+
+print (len(responses))
+
+num_questions=[]
+
+for i, q in enumerate(questions):
+
+    totals = {
+        'Strongly Agree': 0,
+        'Agree': 0,
+        'Neutral': 0,
+        'Disagree': 0,
+        'Strongly Disagree': 0
+    }
+
+    for response in responses:
+
+        answer = response[i+1]
+
+        if answer:
+            totals[answer] += 1
+
+    num_questions.append(totals)
+
+print(num_questions)
+
+
+
+
